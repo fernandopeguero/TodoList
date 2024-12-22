c1b1271111a8b43933556073f714f79dc4d41555
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-21 22:28:35 -0500

    Add user profile toolbar to page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import "./style.css";
-
-
+import { createContent } from "./TodoList/content";
+import { createSideBar } from "./TodoList/sidebar";
+import { childAppender } from "./util";
 
 const body = document.querySelector("body");
+
+const projects = [
+    {
+        name: "something nice",
+        description: "whatss this project about",
+        completed: false,
+        todos: [
+
+        ]
+    }
+]
+
+
+function initiliazeApp() {
+
+    const sidebar = createSideBar();
+    const content = createContent();
+
+    childAppender(body, sidebar, content);
+    
+}
+
+
+initiliazeApp();
