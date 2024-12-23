7e832ef76af34436c1a363b24cbe608f883f3fb8
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-22 23:42:10 -0500

    Add functionality to create link in li

--- src/util.js
+++ src/util.js
@@ -5,3 +5,20 @@ export function childAppender(parent, ...childs) {
     childs.forEach(child => parent.appendChild(child));
 
 }
+
+export function createListItem(text){
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = "#";
+    a.addEventListener('click' , (e) => {
+        e.preventDefault();
+    })
+    a.textContent = text;
+
+    li.appendChild(a);
+
+    return li;
+
+
+}
