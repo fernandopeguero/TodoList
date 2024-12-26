18d397075a11a5c74845529706b15e6b60575d2c
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-25 19:30:08 -0500

    Add icon to the project list

--- src/TodoList/optionList.js
+++ src/TodoList/optionList.js
@@ -1,16 +1,19 @@
 import { childAppender, createListItem } from "../util";
 
-import plusIcon from '../Resources/svg/plus.svg'
+import plusIcon from "../Resources/svg/plus.svg";
+import inboxIcon from "../Resources/svg/inbox.svg";
+import searchIcon from "../Resources/svg/search.svg";
+import upcomingIcon from "../Resources/svg/upcoming.svg";
 
 export function createOptionList() {
 
     const list = document.createElement("ul");
     list.classList.add("options");
 
-    const addTask = createListItem( "Add task", () => { console.log("Adding a task to selected option")})
-    const search = createListItem("Search", () => { console.log("Searching for task.")});
-    const inbox = createListItem("Inbox", () => { console.log("Inbox Task.")});
-    const upcoming = createListItem("Upcoming", () => {console.log("Upcoming projects")})
+    const addTask = createListItem(plusIcon, "Add task", () => { console.log("Adding a task to selected option")})
+    const search = createListItem( searchIcon, "Search", () => { console.log("Searching for task.")});
+    const inbox = createListItem(inboxIcon, "Inbox", () => { console.log("Inbox Task.")});
+    const upcoming = createListItem(upcomingIcon, "Upcoming", () => {console.log("Upcoming projects")})
 
     childAppender(list, addTask, search, inbox, upcoming);
 
@@ -26,9 +29,9 @@ export function createProjectList( ){
     title.textContent = "My Projects";
 
     const projectsList = document.createElement("ul");
+    projectsList.classList.add("project_list");
 
     childAppender(container, title, projectsList);
-
 
     return container;
 }
