14228b0c9834f6ba86bc4233e7aa3856c7133894
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-28 21:55:49 -0500

    Add new logic to display app

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import "./style.css";
-import { createContent } from "./TodoList/content";
-import { createSideBar } from "./TodoList/sidebar";
-import { childAppender } from "./util";
 
-const body = document.querySelector("body");
-
-const projects = [
-    {
-        name: "something nice",
-        description: "whatss this project about",
-        completed: false,
-        todos: [
-
-        ]
-    }
-]
+import { screenController } from "./util";
 
 
-function initiliazeApp() {
-
-    const sidebar = createSideBar();
-    const content = createContent();
-
-    childAppender(body, sidebar, content);
-    
-}
-
-
-initiliazeApp();
+screenController().createApp();
